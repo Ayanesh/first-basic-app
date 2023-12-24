95ab12537d3dae44950c37803317d72e16c9634c
Ayanesh Sinha <sinhaayanesh001@duck.com>
2023-12-24 16:47:23 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import { View,Text, SafeAreaView, StyleSheet } from 'react-native';
+
+import React from 'react'
+
+function App() {
+  return (
+  <View>
+    <Text>
+
+
+    </Text>
+
+    </View>
+  )
+}
+
+export default App;
